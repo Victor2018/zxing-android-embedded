689f5dfa41fa5dc6d6ccc440fd0ef4a548ad8ac4
victor <zhanghaifeng903@sina.com>
2024-10-10 08:27:51 +0800

    add decode listener

---
+++ zxing-android-embedded/src/com/journeyapps/barcodescanner/interfaces/OnZxingDecodeListener.java
@@ -0,0 +1,7 @@
+package com.journeyapps.barcodescanner.interfaces;
+
+import com.journeyapps.barcodescanner.SourceData;
+
+public interface OnZxingDecodeListener {
+    void OnZxingDecode(SourceData sourceData);
+}
